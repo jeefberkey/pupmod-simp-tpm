c2238d46c5ed68198b4df938ee2caddb294bfe1a
Nick Miller <nick.miller@onyxpoint.com>
2016-07-28 09:21:22 -0400

    (SIMP-1286) Update TPM functionality
    - has_tpm now checks new tpm sys path
    - new structured fact, `tpm` contains
      - status of tpm from `sys`
      - pubek
      - output of tpm_version
    - switched to strings-style documentation
    - started work on the tpm type/provider
      - provider can now take ownership of a tpm
      - provider also drops a flat file in $vardir/simp/password that is
        used by the advanced facts
    - provided a profile, `tpm::ownership`
    - wrote a README
    - tests for type/provider
    
    SIMP-1286 #close
    SIMP-1386 #close

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,66 @@
-# == Class: tpm
-#
 # Provides utilities for interacting with a TPM
 #
-# == Parameters
+# @param use_ima [Boolean] Toggles IMA on or off.
 #
-# [*use_ima*]
-#   Boolean.  Toggles IMA on or off.
+# @param enable_pkcs_interface [Boolean] Use the TPM as a PKCS #11 interface
 #
-# == Authors
+# @param take_ownership [Boolean] Enable to allow Puppet to take ownership
+#   of the TPM.
 #
-# * Nick Markowski <nmarkowski@keywcorp.com>
+# @author Nick Markowski <nmarkowski@keywcorp.com>
+# @author Nick Miller <nick.miller@onyxpoint.com>
 #
 class tpm (
-  $use_ima = false
+  $use_ima               = false,
+  $enable_pkcs_interface = false,
+  $take_ownership        = false
 ){
+  validate_bool($use_ima)
+  validate_bool($enable_pkcs_interface)
+  validate_bool($take_ownership)
+
   # Check if the system has a TPM (which also checks that it
   # is a physical machine, and if so install tools and setup
   # tcsd service - uses str2bool because facts return as strings :(
   if str2bool($::has_tpm) {
-    package { 'tpm-tools':
-      ensure => 'present',
-    }
+    package { 'tpm-tools': ensure => latest }
+    package { 'trousers': ensure => latest }
 
     service { 'tcsd':
       ensure  => 'running',
       enable  => true,
       require => Package['tpm-tools'],
     }
+
+    if $enable_pkcs_interface {
+      # TODO
+      # one of these is certainly the one you need
+      # package { 'tpm-quote-tools': ensure => 'present' }
+      # package { 'opencryptoki': ensure => latest }
+      # package { 'opencryptoki-tpmtok': ensure => latest }
+      package { 'tpm-tools-pkcs11': ensure => latest }
+
+      # service { 'pkcsslotd':
+      #   ensure => running,
+      #   enable => true,
+      # }
+      # Initialize SO pin (note that the default SO PIN is 87654321):
+      #   pkcsconf -c 0 -P
+      #
+      # Initialize User pin (use the SO PIN you just defined above):
+      #   pkcsconf -c 0 -u
+      #
+      # Initialize the token:
+      #   pkcsconf -c 0 -I
+    }
+
+    if $take_ownership {
+      include '::tpm::ownership'
+    }
   }
 
   if $use_ima {
     include '::tpm::ima'
   }
+
 }
